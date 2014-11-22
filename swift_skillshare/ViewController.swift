e04cc40f8f46554e325cc8b568c44c8188c0f48e
Joshua Cavin <jroachkilla@yahoo.com>
2014-11-21 21:05:42 -0800

    Initial Commit

---
+++ swift_skillshare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift_skillshare
+//
+//  Created by Joshua Cavin on 11/21/14.
+//  Copyright (c) 2014 Joshua Cavin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
